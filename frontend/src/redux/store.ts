1e133f4caeff8643e37254bbe0fdf9e2786d6a8a
Kelvin Lai <laikelvin8@gmail.com>
2025-02-15 20:50:20 +0000

    Add redux configuration

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,33 @@
+import {configureStore} from "@reduxjs/toolkit"
+import {combineReducers} from "redux"
+import storage from "redux-persist/lib/storage"
+import {
+    persistReducer
+  } from "redux-persist"
+
+  const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+  }
+
+  const reducers = combineReducers({
+
+   })
+
+
+   const persistedReducer = persistReducer(persistConfig, reducers)
+
+   export const store = configureStore({
+     reducer: persistedReducer,
+     devTools: process.env.NODE_ENV !== "production",
+     middleware: (getDefaultMiddleware) =>
+     getDefaultMiddleware({
+       immutableCheck: false,
+       serializableCheck: false
+     }),
+   
+   })
+   
+   export type RootState = ReturnType<typeof store.getState>
+   export type AppDispatch = typeof store.dispatch
