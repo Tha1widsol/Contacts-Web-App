bd848ef6dad3a43f321faf0dbcf715b83e61c414
Kelvin Lai <laikelvin8@gmail.com>
2025-02-15 12:34:57 +0000

    Add new contact model

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    surname = models.CharField(max_length=255, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    mobile = models.CharField(max_length=15, blank=True, null=True)  
+    
+    def __str__(self):
+        return self.first_name
